21bb77495a156207e2ad8aa687598c9a3d657c09
Ryan <etrigan976@gmail.com>
2025-01-23 14:32:41 -0800

    Automatic Git Push for Interpreter Project

--- project/interp.py
+++ project/interp.py
@@ -104,25 +104,4 @@ def run(e: ExpressionType) -> None:
     except evaluate_error as err:
         print(err)
 
-# testing area
-a : ExpressionType = Let('x', Add(Lit(1), Lit(2)), 
-                    Sub(Name('x'), Lit(3)))
-print(a)
 
-b : ExpressionType = Let('x', Lit(1), 
-                    Let('x', Lit(2), 
-                             Mul(Name('x'), Lit(3))))
-print(b)  
-
-c : ExpressionType = Add(Let('x', Lit(1), 
-                        Sub(Name('x'), Lit(2))),
-               Mul(Name('x'), Lit(3)))
-print(c)
-
-d : ExpressionType = Add(Lit(1), Div(Lit(2), Lit(0)))
-print(d)
-
-run(a)
-run(b)
-run(c)
-run(d)
--- test.py
+++ test.py
@@ -1,1 +1,23 @@
 from project.interp import *
+# testing area
+a : ExpressionType = Let('x', Add(Lit(1), Lit(2)), 
+                    Sub(Name('x'), Lit(3)))
+print(a)
+
+b : ExpressionType = Let('x', Lit(1), 
+                    Let('x', Lit(2), 
+                             Mul(Name('x'), Lit(3))))
+print(b)  
+
+c : ExpressionType = Add(Let('x', Lit(1), 
+                        Sub(Name('x'), Lit(2))),
+               Mul(Name('x'), Lit(3)))
+print(c)
+
+d : ExpressionType = Add(Lit(1), Div(Lit(2), Lit(0)))
+print(d)
+
+run(a)
+run(b)
+run(c)
+run(d)
