6c416d32741d236b7d062aebe19732b1547d3435
Ryan <etrigan976@gmail.com>
2025-01-27 20:17:11 -0800

    Automatic Git Push for Interpreter Project

--- project/interp.py
+++ project/interp.py
@@ -1,92 +1,139 @@
 # Interpreter that does boolean operations and normal arithmetic
 # Module imports for processing
+# dataclasses automatically create initializers for marked classes
 from dataclasses import dataclass
+# LiteralVal marks the classes that can be utilized for literal or plain values
 type LiteralVal = int | bool | str
+# ExpressionType marks the available functions to utilize for the interpreter AST
 type ExpressionType = Add | Sub | Mul | Div | Neg | Lit | Let | Name | And | Or | Not | Eq | NEq | Lt | LtE | Gt | GtE | If | Append | Replace
 # Integer Literal Arithmetic
+# Add() accepts 2 integers for addition
+# ex: 4 + 5 = 9
+# ex: Add(Lit(4), Lit(5)) = 9
 @dataclass
 class Add():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} + {self.right_val})"
+# Sub() accepts 2 integers for subtraction
+# ex: 6 - 5 = 1
+# ex: Sub(Lit(6), Lit(5)) = 1
 @dataclass
 class Sub():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} - {self.right_val})"
+# Mul() accepts 2 integers for multiplication
+# ex: 7 * 8 = 56 
+# ex: Mul(Lit(7), Lit(8)) = 56
 @dataclass
 class Mul():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} * {self.right_val})"
+# Div() accepts 2 integers for division
+# throws an error upon 0 denominator
+# ex: 6 / 2 = 3
+# ex: Div(Lit(6), Lit(2)) = 3
 @dataclass
 class Div():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} / {self.right_val})"
+# Neg() accepts 1 integer for negation
+# flips the value of a positive to a negative and vice versa
+# ex: Neg(Lit(6)) = -6
 @dataclass
 class Neg():
     subexpression: ExpressionType
     def __str__(self) -> str:
         return f"(- {self.subexpression})"
 # Boolean Arithmetic
+# And() accepts 2 booleans for the and comparison
+# ex: True && False = False
+# ex: And(Lit(True), Lit(False)) = False
 @dataclass 
 class And():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} && {self.right_val})"
+# Or() accepts 2 booleans for the or comparison
+# ex: True || False = True
+# ex: Or(Lit(True), Lit(False)) = True
 @dataclass 
 class Or():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} || {self.right_val})"
+# Not() accepts 1 boolean and flips from True to False and vice versa
+# ex: !True = False
+# ex: Not(Lit(True)) = False
 @dataclass
 class Not():
     subexpression: ExpressionType
     def __str__(self) -> str:
         return f"(NOT {self.subexpression})"
+# Eq() accepts 2 Literals and compares them to each other
+# ex: 2 == 3 = False
+# ex: Eq(Lit(2), Lit(3)) = False
 @dataclass
 class Eq():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} == {self.right_val})"
+# NEq() accepts 2 Literals and compares them to each other
+# ex: 2 != 3 = True
+# ex: NEq(Lit(2), Lit(3)) = True
 @dataclass
 class NEq():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} != {self.right_val})"
+# Lt() accepts 2 integers and compares them to each other
+# ex: 2 < 3 = True
+# ex: Lt(Lit(2), Lit(3)) = True
 @dataclass
 class Lt():
     left_val:ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} < {self.right_val})"
+# LtE() accepts 2 integers and compares them to each other
+# ex: 2 <= 3 = True
+# ex: LtE(Lit(2), Lit(3)) = True
 @dataclass
 class LtE():
     left_val:ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} <= {self.right_val})"
+# Gt() accepts 2 integers and compares them to each other
+# ex: 2 > 3 = False
+# ex: Gt(Lit(2), Lit(3)) = False
 @dataclass
 class Gt():
     left_val:ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} > {self.right_val})"
+# GtE() accepts 2 integers and compares them to each other
+# ex: 2 >= 3 = False
+# ex: GtE(Lit(2), Lit(3)) = False
 @dataclass
 class GtE():
     left_val:ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} >= {self.right_val})"
+# If() accepts 3 expressions for a condition, a primary branch and a secondary branch
 @dataclass
 class If():
     condition: ExpressionType
@@ -95,12 +142,15 @@ class If():
     def __str__(self) -> str:
         return f"(if {self.condition} \n\tthen {self.then_sect} \n\telse {self.else_sect})"
 # Domain-specific Library functions and implementation
+# Append merges 2 strings of characters and returns the value
 @dataclass
 class Append():
     left_val: ExpressionType
     right_val: ExpressionType
     def __str__(self) -> str:
         return f"({self.left_val} += {self.right_val})"
+# Replace() takes an initial string and replaces the first instance of a given 
+# string and replaces it with another given string
 @dataclass
 class Replace():
     initial_string: ExpressionType
